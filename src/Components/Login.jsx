55fdd4331c4ed991c065b46e1ddfcd723cede2ab
anas-choudhary18 <anasch6399@gmail.com>
2025-07-10 13:37:34 +0530

    ADD!

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,7 +5,7 @@ import Template from './Template';
 function Login({ setIsLoggedIn }) {
   return (
     <Template
-      title="Welcome Back"
+      title="Welcome Back" 
       desc1="Build skills for today, tomorrow, and beyond."
       desc2="Education to future-proof your career."
       image={loginImg}
